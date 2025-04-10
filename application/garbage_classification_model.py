5c307a955cfc14b41fec4a9cf20b56ec835a310a
honyakgergo <139636823+honyakgergo@users.noreply.github.com>
2025-04-10 16:48:11 +0200

    Update garbage_classification_model.py

--- application/garbage_classification_model.py
+++ application/garbage_classification_model.py
@@ -1,4 +1,4 @@
-
+import random
 import numpy as np
 import os
 
@@ -15,3 +15,8 @@ def predict_class(image):
     class_labels = {0: "Food_Organics", 1:'Glass', 2:'Metal', 3:'Paper_Cardboard', 4:'Plastic',  5:'Vegetation'}
     return class_labels.get(predicted_class, "Unknown")
 
+def predict_class(image):
+    prediction = np.array([[random.uniform(0, 1) for i in range(6)]])
+    predicted_class = np.argmax(prediction, axis=1)[0]
+    class_labels = {0: "Food_Organics", 1:'Glass', 2:'Metal', 3:'Paper_Cardboard', 4:'Plastic',  5:'Vegetation'}
+    return class_labels.get(predicted_class, "Unknown")
