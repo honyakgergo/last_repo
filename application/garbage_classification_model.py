ef21429502b2dc714e672450d4b8a3abf03095b3
honyakgergo <139636823+honyakgergo@users.noreply.github.com>
2025-04-10 16:47:13 +0200

    Update garbage_classification_model.py

--- application/garbage_classification_model.py
+++ application/garbage_classification_model.py
@@ -1,16 +1,12 @@
-import tensorflow as tf
+
 import numpy as np
 import os
 
-def load_model():
-    base_dir = os.path.dirname(os.path.abspath(__file__))  
+ 
     
 
-    model = tf.keras.models.load_model("application/garbage_classification_model.keras", compile=False)
-    print(model.summary())
-    return model
 
-def predict_class(model, image):
+def predict_class(image):
     prediction = model.predict(image)
     print(prediction)
     predicted_class = np.argmax(prediction, axis=1)[0]
